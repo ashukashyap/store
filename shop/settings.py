c288a220614e71c24be5ed8d37251d55c38bc028
ashu <aashuk012@gmail.com>
2021-06-03 01:36:59 +0530

    chang

--- shop/settings.py
+++ shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'ih_#ij4xnd5b2)2y@%)ousb2$xkdoamf90)y(&#f1g5ri&!yi&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['143.198.166.234']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogdb',
-        'USER': 'blog_admin',
-        'PASSWORD': 'testing123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'blogdb',
+#         'USER': 'blog_admin',
+#         'PASSWORD': 'testing123',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
