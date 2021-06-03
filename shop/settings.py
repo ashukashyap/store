876715828f8d9a880043e60e167521994199949d
ashu <aashuk012@gmail.com>
2021-06-03 10:12:16 +0530

    lkjs

--- shop/settings.py
+++ shop/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
 
 
 
